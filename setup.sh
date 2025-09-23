2dd7dd7db94b86f80ecdec0e47a099d6466e072d
Your Name <you@example.com>
2025-09-23 11:25:09 -0400

    -

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 
 
 # Bootstrap python environment
+echo "Setup Python"
 python3.11 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
+echo "python setup done"
 
+
+cp env-sample .env
+
+git config --global user.email "you@example.com"
+git config --global user.name "Your Name"
 
 # Enable backend services
 echo "Configuring systemctl"
